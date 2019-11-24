ca7f44bb42c3bf41a9910a7f03530c792d0e66d2
xiayue <xiayue@casachina.com.cn>
2019-11-24 12:07:54 +0800

    test

---
+++ src/test/java/pers/xy/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.xy.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
